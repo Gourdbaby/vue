68a0d2b852a8cbc9f2a383ed155cddf593a2892c
sunminxiang <minxiang.sun@zhaopin.com.cn>
2017-03-28 10:11:27 +0800

    go on

---
+++ components/example.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		sddasdas
+	</div>
+</template>
+
+<script>
+export default {
+    data (){
+        return {
+            emit: ""
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
